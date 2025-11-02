4b312f3229764d2b933dcf2fd3bfc390be574a70
Vlad <124621507+Dialorsd@users.noreply.github.com>
2025-11-02 22:51:31 +0200

    Commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask 
+from flask import Flask, render_template
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
-@app.route('/') 
-def main():
-    return 'Hello, world!'
+@app.route('/')
+def resume():
+    return render_template('resume.html', title='Резюме')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title='Контакти')
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
